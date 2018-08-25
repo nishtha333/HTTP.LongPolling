5949be57dd9fb590678f0a09c8902e42030c848a
Nishtha Malhotra <nishthamalhotra@gmail.com>
2018-08-25 17:39:26 -0400

    Lab

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const { EventEmitter } = require('events');
+const app = express();
+
+const clock = new EventEmitter();
+setInterval(() => {
+    const time = (new Date()).toLocaleString()
+    clock.emit('tick', time)
+}, 5000);
+
+clock.on('tick', time => console.log(`The time is ${time}`));
+
+app.get('/', (req, res, next) => {
+    res.send(`
+        <html>
+            <head>
+                <script type="text/javascript">
+                    console.log('hello world!')
+                </script>
+            </head>
+        </html>
+    `)
+});
+
+app.get('/the-time', (req, res, next) => {
+    clock.once('tick', time => res.send(time));
+});
+
+app.listen(3333, () => {
+    console.log(`Listening on port 3333...`)
+});
+
